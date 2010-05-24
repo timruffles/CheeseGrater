fa63301a29a3c4d764c225b2f2d4e3d002eab87c
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-05-24 14:10:55 +0100

    Revert "Added CSV export"
    This reverts commit 475506cb3eda8e6fd251f25f5f240536865f368f.

--- script/biz_scraper_cli.rb
+++ script/biz_scraper_cli.rb
@@ -1,11 +1,8 @@
 require 'yaml'
-require 'csv'
-FILEDIR = File.dirname(__FILE__) + '/'
-require "#{FILEDIR}../lib/eventscraper"
+ROOT = '/Users/timruffles/Development/Cothink/spikes/EventsScraper/'
+require "#{ROOT}lib/eventscraper"
 
-output_file = "#{FILEDIR}../output/output.csv"
-
-s = BusinessLink.new("#{FILEDIR}../spec/eventscraper/scrapers/fixtures/business_link.html")
+s = BusinessLink.new("#{ROOT}spec/eventscraper/scrapers/fixtures/business_link.html")
 
 l2 = []
 s.scrape do |yielded|
@@ -19,15 +16,10 @@ l2.each do |l2|
   end
 end
 
-for_csv = []
-events.select { |e| e.respond_to? :title}.each do |event|
-  order = [:title, :description, :date, :location, :organiser, :link, :source]
-  for_csv << order.collect do |data|
-    event.send(data)
-  end
+#puts events.select { |e| e.respond_to? :title}.first
+events.select { |e| e.respond_to? :title}.each do |e|
+  es = EventSaver.new
+  es.perform(Marshal.dump(e))
 end
-CSV.open("#{FILEDIR}../output/output.csv", "w") do |csv|
-  for_csv.each do |line|
-    csv << line
-  end 
-end
+
+
