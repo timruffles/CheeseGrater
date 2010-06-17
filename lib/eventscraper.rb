df92c49add7799be9eaec4f7b6a178e9fb583349
Daniel Morris <dan@Belobog.local>
2010-06-17 13:08:52 +0100

    comment out loading of non existent file

--- lib/eventscraper.rb
+++ lib/eventscraper.rb
@@ -4,7 +4,9 @@ require File.dirname(__FILE__) + '/eventscraper/api_scraper'
 require File.dirname(__FILE__) + '/eventscraper/url_scraper'
 require File.dirname(__FILE__) + '/eventscraper/scraper_runner'
 require File.dirname(__FILE__) + '/eventscraper/event_saver'
-require File.dirname(__FILE__) + '/eventscraper/querystring_api_scraper'
+
+# no file exists yet!
+#require File.dirname(__FILE__) + '/eventscraper/querystring_api_scraper'
 
 scrapers_lib_dir = File.dirname(__FILE__) + '/eventscraper/scrapers'
 Dir["#{scrapers_lib_dir}/*.rb"].each { |f| load(f) }
