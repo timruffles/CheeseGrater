3f967ed0e1060052878b951de22c3c01769c86ae
Daniel Morris <dan@Belobog.local>
2010-06-17 13:10:26 +0100

    meh

--- spec/eventscraper/api_scraper_spec.rb
+++ spec/eventscraper/api_scraper_spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/../../lib/eventscraper'
-describe ApiScraper do
+
+describe ApiScraper do  
   
   it "should turn a list of params into urls" do
-    
+
     ApiScraper :querystring, { :topic => {:values=>['business','networking skills'],:type=>:one_per_request},
                                :country => {:values => %w[gb us fr], :type => :csv} }
-    
     
   end
   
