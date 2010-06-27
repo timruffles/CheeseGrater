daba4d3f0633a18bd157377db2e2873224b5fc35
Tim Ruffles <timruffles@Tims-Laptop.lan>
2010-06-27 12:36:01 +0100

    About to switch pager to Kwalify - no point rewriting later

--- lib/cheesegrater/pager.rb
+++ lib/cheesegrater/pager.rb
@@ -3,18 +3,48 @@ module CheeseGrater
     
     class << self
       def create setup
-        Pager.new
+        pager = Pager.new
+        setup.each_pair do |name, val|
+          pager.send("#{name}=".to_sym,val)
+        end
+        pager
       end
     end
     
     def page
       
-      first_page_response = yield({})
-      while fields = read_fields(first_page_response)
-         yield fields
+      # first page
+      response = yield({})
+      
+      # all other pages
+      while read_fields(response) != {} 
+         response = yield 
       end
       
     end
+    
+    
+    attr_accessor :next_page_url_path
+    
+    protected
+    
+    
+    def read_fields response
+      fields = {}
+      pager_fields = {
+        :endpoint => :next_page_url_path
+      }
+      pager_fields.each_pair do |field_to_yield, path_accessor|
+        
+        field_path = self.send path_accessor
+        value = response.value(field_path) if field_path
+        next unless value
+        fields[field_to_yield] = value
+        
+      end
+      fields
+    end
+    
     
   end
 end
--- spec/cheesegrater/pager_spec.rb
+++ spec/cheesegrater/pager_spec.rb
@@ -3,34 +3,40 @@ require root + '/spec_helper'
 
 describe CheeseGrater::Pager do
   
-  it "should expose a method to reset its state"
-  
   context "given an empty setup" do
     it "should yield a null page fields" do
       
       pager = CheeseGrater::Pager.create({})
       yielded = []
       
-      pager.page({}) do |fields|
+      pager.page do |fields|
         
-        
+        fields.should == {}
         
       end
-      
-      yielded.length.should == 1
       
     end
   end
   
   context "only given a next page url" do
     it "should be able to yield requests until no url is given" do
+      
+      url_path = '//blah'
       pager = CheeseGrater::Pager.create({:request => 
-                                            {:next_page_url_path => '//blah'}
+                                            {:next_page_url_path => url_path}
                                         })
+                                        
+      response = mock('Response')
+      response.should_receive(:value).with(url_path).and_return('http://example')
+      
       yielded = []
-      pager.each_page_fields do |fields|
-        fields.should == {}
+      pager.page do |fields|
+        
+        fields.should == {:next_page_url_path=>'http://example'}
         yielded << fields 
+        
+        response.should_receive(:value).with(url_path).and_return('')
+        response
       end
       yielded.length.should == 1
     end
