c834ada293ad3772065c103ae76c926fbe18d2e4
Tim Ruffles <timruffles@Tims-Laptop.local>
2010-06-28 17:36:50 +0100

    Added specs to scraper, after writing a proposed implementation

--- spec/cheesegrater/scraper_spec.rb
+++ spec/cheesegrater/scraper_spec.rb
@@ -8,6 +8,13 @@ describe CheeseGrater::Scraper do
   before :each do
     @scraper = CheeseGrater::Scraper.new({})
     @fixtures = YAML.load_file(root + '/fixtures/scraper.yml').keys_to_symbols
+    @xpath_response = Response::XpathHtml.new
+    
+    @vo_for_fixture_one = [Vo.new({:fields => 
+                      {:location_code => "./@value"},
+                  :related_to => {},
+                  :item_path => "//*[@id='location']/*[@value!='-999']"})]
+    
   end
   
   it "should be dumpable" do
@@ -16,7 +23,7 @@ describe CheeseGrater::Scraper do
   end
   
   it "should run requests and yield the raw responses" do
-    fields = {}
+    
     response_msg = 'Response read!'
     
     request = mock('Request')
@@ -27,69 +34,113 @@ describe CheeseGrater::Scraper do
     @scraper.send(:make_requests, [request], CheeseGrater::Pager.new) do |raw_response|
       responses << raw_response
     end
-
     responses.length.should == 1
     responses[0].should == response_msg
   end
   
   context "integration" do
     
-    it "should yield scrapers" do
+    it "should yield related scrapers" do
       
-      response = Response::XpathHtml.new
-      response.raw = @fixtures[:scrapers]
+      @xpath_response.raw = @fixtures[:scrapers]
       
       related_scrapers = {
-        'ScraperOne' => Scraper.new(:requests         => [Request::Base.new({:fields=>{:app_key => 'blah', :date => 'x'},:endpoint=>'x'})],
-                                    :response         => Response::XpathHtml.new,
-                                    :vos              => Vo.new({}),
-                                    :pager            => Pager.new,
-                                    :is_root          => true,
-                                    :related_scrapers => {},
-                                    :scrapers         => {})
-      }
-      
+                      'ScraperOne' => Scraper.new(:requests         => [Request::Base.new({:fields=>{:app_key => 'blah', :date => 'x'},:endpoint=>'x'})],
+                                                  :response         => Response::XpathHtml.new,
+                                                  :vos              => Vo.new({}),
+                                                  :pager            => Pager.new,
+                                                  :is_root          => true,
+                                                  :related_scrapers => {},
+                                                  :scrapers         => {})
+                    }
       scrapers = {
         'ScraperOne' => {:item_path => "//*[@id='eventDate']//*[@value!=99]", :fields => {:date=>"./@value"}}
       }
       
       results = []
-      @scraper.send(:read_response, [], response, related_scrapers, scrapers) do |scraped|
+      @scraper.send(:read_response, [], @xpath_response, related_scrapers, scrapers) do |scraped|
         results << scraped
       end
       
-      total_of_vals = results.inject(0) do |acc, scraper|
+      (total_of_vals = results.inject(0) do |acc, scraper|
         acc += scraper.requests.inject(0) {|acc, r| acc += r.fields[:date].to_i}
-      end
+      end).should == 153
       
       results.select do |scraper|
         scraper.requests[0].fields[:app_key].should == 'blah'
       end
-      
       results.length.should == 18
-      total_of_vals.should == 153
       
     end
   
     it "should yield vos" do
       
-      vos = [Vo.new({:fields => {:location_code => "./@value"},:related_to => {},:item_path => "//*[@id='location']/*[@value!='-999']"})]
-      response = Response::Xpath.new
-      response.raw = @fixtures[:one]
+      @xpath_response.raw = @fixtures[:one]
       
       results = []
-      @scraper.send(:read_response, vos, response) do |scraped|
+      @scraper.send(:read_response, @vo_for_fixture_one, @xpath_response) do |scraped|
         results << scraped
       end
       
-      total_of_vals = results.inject(0) do |acc, vo|
+      (total_of_location_codes = results.inject(0) do |acc, vo|
         acc += vo.fields[:location_code].to_i
-      end
+      end).should == 110
       
-      results.length.should > 0
-      total_of_vals.should == 110
     end
     
+    it "should yield related scrapers"
+    
+    context "given a vo with a list of related vos" do
+    
+      context "when the vo data is present on the same page" do
+        
+        it "should only yield the vo with related vos stored inside it with all data" do
+          
+          @xpath_response.raw = @fixtures[:related_vos_on_page]
+          
+          vos = Vo.create_all YAML.load(<<-YAML
+            Event:
+              item_path
+          
+          YAML
+          ).keys.to_symbols
+          
+          @scraper.send(:read_response, vos, @xpath_response) do |scraped|
+            results << scraped
+          end
+          results.length.should == 1
+          results.related_to.length.should == 2
+          results.related_to[0].fields.should == {:name => 'John Doe', :url => 'http://blah'}
+        end
+        
+      end
+      
+      context "when the vo data is on another page" do
+        
+        it "should yield each of the related vo scrapers with the original vo's UUID in their relations'" do
+          
+          @xpath_response.raw = @fixtures[:related_vos_external]
+          
+          vos = Vo.create_all YAML.load(<<-YAML
+            Event:
+              item_path
+          
+          YAML
+          ).keys.to_symbols
+          vo = vos[0]
+          vo.make_uuid!
+          
+          @scraper.send(:read_response, vos, @xpath_response) do |scraped|
+            results << scraped
+          end
+          results.length.should == 2
+          results.each {|scraper| scraper.vos[:Organiser].related_to[:Event].should == vo.uuid}
+          
+        end
+        
+      end
+      
+    end
     
   end
   
